0e93052f6d1e9665f27d28f44616e8cd2b733d8b
Elliot Block <elliot@deck36.net>
2015-06-14 22:52:46 -0700

    Enable branching based on HTTP method

--- src/main/scala/Wingnut.scala
+++ src/main/scala/Wingnut.scala
@@ -18,6 +18,16 @@ object HttpMethod {
   case object PUT extends HttpMethod
   case object POST extends HttpMethod
   case object DELETE extends HttpMethod
+
+  def fromString(str : String) : Option[HttpMethod] = {
+    str.toLowerCase match {
+      case "get" => Some(GET)
+      case "put" => Some(PUT)
+      case "post" => Some(POST)
+      case "delete" => Some(DELETE)
+      case _ => None
+    }
+  }
 }
 
 case class Request(
@@ -32,22 +42,33 @@ case class Response(
 
 class JettyServer(port: Int, handler: (Request) => Response) {
   class JettyHandler extends AbstractHandler {
-    def convertServletRequest(r: HttpServletRequest): Request = {
+
+    def convertServletRequest(r: HttpServletRequest): Option[Request] = {
       val reader = r.getReader()
       val str = Stream.continually(reader.readLine()).
         takeWhile(_ != null).
         mkString("\n")
-      Request(Array(), HttpMethod.GET, str)
+
+      val method = HttpMethod.fromString(r.getMethod)
+      if (method.isEmpty) {
+        None
+      }
+      else {
+        Some(Request(Array(), method.get, str))
+      }
     }
 
     def handle(target: String,
                baseRequest: JettyRequest,
                request: HttpServletRequest,
                response: HttpServletResponse) {
-      val r = handler(convertServletRequest(request))
-      response.setStatus(r.status)
+      val resp = convertServletRequest(request) match {
+        case Some(req) => handler(req)
+        case None => Response("Not goody", 400)
+      }
+      response.setStatus(resp.status)
       baseRequest.setHandled(true)
-      response.getWriter.println(r.body)
+      response.getWriter.println(resp.body)
     }
   }
 
@@ -61,9 +82,22 @@ class JettyServer(port: Int, handler: (Request) => Response) {
 object Wingnut {
   val logger = Logger(LoggerFactory.getLogger("name"))
 
-  def shmucker(r: Request): Response = {
-    logger.info(r.body)
+  def shmuckerGet(r: Request): Response = {
+    Response("GET!", 200)
+  }
+
+  def shmuckerPost(r: Request): Response = {
+    val df = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    logger.info(df.format(new java.util.Date()) + "\t" + r.body)
     Response("Foey!", 200)
+  }
+
+  def shmucker(r: Request): Response = {
+    r.method match {
+      case HttpMethod.GET => shmuckerGet(r)
+      case HttpMethod.POST => shmuckerPost(r)
+      case _ => Response("Method not allowed", 405)
+    }
   }
 
   def main(argv: Array[String]) {
