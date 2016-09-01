3ba921388363f6a60f67485c0d0b484b7b04e57e
Elliot Block <elliot@deck36.net>
2016-09-01 13:34:37 -0700

    Rename IndexHandler

--- src/main/scala/com/linkypants/Linkypants.scala
+++ src/main/scala/com/linkypants/Linkypants.scala
@@ -9,7 +9,7 @@ import siesta.jetty.JettyServer
 
 import com.linkypants.view
 
-object Handler {
+object IndexHandler {
   val logger = Logger(LoggerFactory.getLogger("name"))
 
   def content(): Array[String] = {
@@ -20,11 +20,11 @@ object Handler {
     }
   }
 
-  def shmuckerGet(r: Request): Response = {
+  def get(r: Request): Response = {
     Response(view.IndexView.view(content()), Array(ContentType("text/html")), 200)
   }
 
-  def shmuckerPost(r: Request): Response = {
+  def post(r: Request): Response = {
     val df = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
     logger.info(df.format(new java.util.Date()) + "\t" + r.body)
     this.synchronized {
@@ -38,10 +38,10 @@ object Handler {
     Response("Foey!", Array(Location("/"), ContentType("text/html")), 303)
   }
 
-  def shmucker(r: Request): Response = {
+  def handler(r: Request): Response = {
     r.method match {
-      case Method.GET => shmuckerGet(r)
-      case Method.POST => shmuckerPost(r)
+      case Method.GET => get(r)
+      case Method.POST => post(r)
       case _ => Response("Method not allowed", Array(), 405)
     }
   }
@@ -49,7 +49,7 @@ object Handler {
 
 object Wingnut {
   def main(argv: Array[String]) {
-    val s = new JettyServer(8080, Handler.shmucker)
+    val s = new JettyServer(8080, IndexHandler.handler)
     s.start
     s.join
   }
